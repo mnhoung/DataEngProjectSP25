21bab943392f6f0c624c1fcfbc9f4e5163eb713d
Molina Nhoung <nhoung@pdx.edu>
2025-04-18 01:37:27 +0000

    finished data pub

--- dataPublisher.py
+++ dataPublisher.py
@@ -3,47 +3,56 @@ from google.oauth2 import service_account
 import json
 import os
 
-project_id = "sp25-cs410-trimet-project"
-topic_id = "trimet-topic"
+PROJECT_ID = "sp25-cs410-trimet-project"
+TOPIC_ID = "trimet-topic"
+SERVICE_ACCOUNT_FILE = "/home/pjuyoung/term-project/sp25-cs410-trimet-project-service-account.json"
+BASE_DIR = "/home/pjuyoung/"
 
-publisher = pubsub_v1.PublisherClient()
-# The `topic_path` method creates a fully qualified identifier
-# in the form `projects/{project_id}/topics/{topic_id}`
-topic_path = publisher.topic_path(project_id, topic_id)
-base_dir = "/home/pjuyoung/"
+def future_callback(future):
+    try:
+        # Wait for the result of the publish operation.
+        future.result()  
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
+# Get a credential object from google.oauth2 using your service account file!!! COOL!
+pubsub_creds =  (service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE))
 
-def fetch_data(file_path):
-    # Open and dump the file
-    file_path = os.path.join(file_path, "vehicle_3321_20250416_190052.json")
-    with open(file_path, "r") as file:
-        try:
-            data = json.load(file)
+# Use that credential object to create your publisher object.
+publisher = pubsub_v1.PublisherClient(credentials=pubsub_creds)
+topic_path = publisher.topic_path(PROJECT_ID, TOPIC_ID)
 
-            # Only publish if there's data in the file
-            if data:
-                for obj in data:
-                    json_data = json.dumps(obj).encode("utf-8")
-                    future = publisher.publish(topic_path, json_data)
-                    print(future.result())
-        except Exception as e:
-            print(f"Error occured while loading json: {e}")
-            exit(0)
+future_list = []
+count = 0
+for folder in os.listdir(BASE_DIR):
+    folder_path = os.path.join(BASE_DIR, folder)
 
-def main():
-    '''
-    for folder in os.listdir(base_dir):
-        folder_path = os.path.join(base_dir, folder)
+    # Only open breadcrumb folders
+    if folder.startswith("breadcrumb_data"):
+        # Loop through the json files
+        for filename in os.listdir(folder_path):
+            if filename.startswith("vehicle") and filename.endswith(".json"):
+                file_path = os.path.join(folder_path, filename)
 
-        # Only open breadcrumb folders
-        if folder.startswith("breadcrumb_data"):
-            # Loop through the json files
-            for filename in os.listdir(folder_path):
-                if filename.startswith("vehicle") and filename.endswith(".json"):
-                    file_path = os.path.join(folder_path, filename)
-                    fetch_data(file_path)
-    '''
-    fetch_data("/home/pjuyoung/breadcrumb_data_20250417/")
-    print(f"Published messages to {topic_path}.")
-if __name__ == "__main__":
-    main()
+                # Open and dump the file
+                with open(file_path, "r") as file:
+                    try:
+                        data = json.load(file)
+
+                        # Only publish if there's data in the file
+                        if data:
+                            for obj in data:
+                                json_data = json.dumps(obj).encode()
+                                future = publisher.publish(topic_path, json_data)
+                                future.add_done_callback(future_callback)
+                                future_list.append(future)
+                                count += 1
+                                if count % 50000 == 0:
+                                    print(count)
+
+                    except Exception as e:
+                        print(f"Error occured while loading json: {e}")
+                        exit(0)
+for future in futures.as_completed(future_list):
+    continue
+print(f"Published messages to {topic_path}.")
