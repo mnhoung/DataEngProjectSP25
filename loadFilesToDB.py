f3f7cd42a1f06e92bddf896220eac8a1695fba32
Olivia Zhang <ozhang@pdx.edu>
2025-05-08 22:58:37 -0700

    load one file at a time

--- loadFilesToDB.py
+++ loadFilesToDB.py
@@ -3,30 +3,16 @@ import json
 import os
 from dataLoadToDB import LoadToDB
 
-files = [
-    'recieved_data_20250501.json',
-    'recieved_data_20250502.json',
-    'recieved_data_20250503.json',
-    'recieved_data_20250504.json',
-    'recieved_data_20250505.json',
-    'recieved_data_20250506.json',
-    'recieved_data_20250507.json',
-    'recieved_data_20250508.json'
-]
+json_file = '../recieved_data/recieved_data_20250501.json'
 
-directory = '../recieved_data'
-all_data = []
+with open(json_file, 'r') as file:
+    data = [json.loads(line.strip()) for line in file if line.strip()]
+print("read json")
 
-for filename in files:
-    file_path = os.path.join(directory, filename)
-    with open(file_path, 'r') as f:
-        data = [json.loads(line.strip()) for line in f if line.strip()]
-        all_data.extend(data)
-    print(f"Loaded {len(data)} rows from {filename}")
-
-df = pd.DataFrame(all_data)
+df = pd.DataFrame(data)
+print("created df")
 
 loader = LoadToDB()
 loader.run(df)
 
-print(f"Finished loading {len(df)} records from selected files.")
+print(f"loaded {len(df)} records from {json_file}")
